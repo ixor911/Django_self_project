594bb2b600b2315490ce9a9cfc7f4b1fdfb77375
ixor911 <igorbutenko9111@gmaul.com>
2022-10-05 22:16:28 +0300

    made simple view, and made models

---
+++ taskmanager/main/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import *
+
+
+admin.site.register(Role)
+admin.site.register(Employee)
+admin.site.register(Chat)
+admin.site.register(Task)
+admin.site.register(Message)
+
+
---
+++ taskmanager/main/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+
+
+def home(request):
+    return render(request, 'main/home.html')
+
+
